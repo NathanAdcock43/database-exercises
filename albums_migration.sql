7aee65a89368c4b5f9c5c214a7a5484e831fd4cb
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-18 14:41:43 -0500

    feat: updated albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,15 +1,21 @@
 USE codeup_test_db;
 
-DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100),
     name VARCHAR(100) NOT NULL,
     release_date SMALLINT UNSIGNED,
-    sales FLOAT(6,2),
-    genre VARCHAR(50),
+    sales FLOAT(2),
+    genre VARCHAR(100),
     PRIMARY KEY (id)
     );
 
-SELECT * FROM album;
+SELECT * FROM albums;
+
+
+-- USE codeup_test_db;
+-- SHOW Tables;
+-- Describe albums;
+

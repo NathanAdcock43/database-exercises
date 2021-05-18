eba7f360cec18869226a9a07b374140fbe5ab589
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-18 14:54:02 -0500

    feat: updated albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100),
-    name VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     release_date SMALLINT UNSIGNED,
     sales FLOAT(2),
     genre VARCHAR(100),
@@ -19,3 +19,5 @@ SELECT * FROM albums;
 -- SHOW Tables;
 -- Describe albums;
 
+INSERT INTO albums (artist, title, release_date, sales, genre)
+VALUE ('Pink Floyd', 'The Wall', 1979, 19000000000, 'Progressive Rock');

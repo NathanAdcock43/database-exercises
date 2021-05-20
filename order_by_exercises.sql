fc0fe53373ed51a31ad23b358f4804ba4f5865f2
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-20 14:34:23 -0500

    feat: Still working on ordered by question 4

---
+++ order_by_exercises.sql
@@ -0,0 +1,48 @@
+USE employees;
+-- Setup:Create a file named where_exercises.sql.
+-- Make sure to use the employees database before you begin.
+
+
+-- 1) Modify your first query to order by first name. The first result
+# should be Irena Reutenauer and the last result should be Vidya Demeyer.
+
+SELECT first_name, last_name
+FROM employees
+ORDER BY last_name
+IN ('Irena', 'Vidya', 'Maya');
+
+-- 2) Update the query to order by first name and then last name.
+# The first result should now be Irena Acton and the last should be Vidya Boguraev.
+
+SELECT first_name, last_name
+FROM employees
+ORDER BY last_name
+AND first_name
+IN ('Irena', 'Vidya', 'Maya');
+
+-- 3)Change the ORDER BY clause so that you order by last name before first name.
+# Your first result should still be Irena Acton but now the last result should be Vidya Quittner.
+
+SELECT first_name, last_name
+FROM employees
+Where last_name
+          like '%q%';
+
+-- 4) Update your queries for employees with 'E' in their last name to sort
+#  the results by their employee number. Your results should not change!
+
+SELECT first_name, last_name
+FROM employees
+Where last_name
+    like 'e%'
+   OR '%e';
+
+-- 5) Now reverse the sort order for both queries and compare results.
+
+SELECT first_name, last_name
+FROM employees
+Where last_name
+    like 'e%'
+  AND '%e';
+
+
